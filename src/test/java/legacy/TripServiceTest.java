d2ba6cf7f30828a1e179ce9ce7f3a3eb3e845c2e
이정인 <aretias.kr@gmail.com>
2013-09-23 03:24:45 +0900

    TestableTripService의 getLoggedInUser를 TestableTripService에서 정해진 값으로 전달하는 것이 아니라 테스트케이스에서 선택해서 넘길 수 있도록 한다.

--- src/test/java/legacy/TripServiceTest.java
+++ src/test/java/legacy/TripServiceTest.java
@@ -7,16 +7,21 @@ import lagacy.UserNotLoggedInException;
 import org.junit.Test;
 
 public class TripServiceTest {
+	private static final User GUEST = null;
+	private static final User UNUSED_USER = null;
+	private User loggedInUser;
+
 	@Test(expected = UserNotLoggedInException.class)
 	public void should_throw_an_exception_when_user_is_not_logged_in() throws Exception {
 		TripService tripService = new TestableTripSerivce();
-		tripService.getTripsByUser(null);
+		loggedInUser = GUEST;
+		tripService.getTripsByUser(UNUSED_USER);
 	}
 	
 	private class TestableTripSerivce extends TripService {
 		@Override
 		protected User getLoggedInUser() {
-			return null;
+			return loggedInUser;
 		}
 	}
 }
